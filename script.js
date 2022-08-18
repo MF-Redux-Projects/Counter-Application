145b3575ce80d45fcc624a6fecbfbd9769cb1a27
mehedifoysal <mehedifoysal8@gmail.com>
2022-08-18 22:34:48 +0600

    initial state and select dom elements

---
+++ script.js
@@ -0,0 +1,21 @@
+//Select Dom Element
+const counterWrapperEl = document.getElementById("counter-wrapper");
+const counterCloneEl = document.getElementById("counter-clone");
+const addCounterEl = document.getElementById("add-counter");
+const resetEl = document.getElementById("reset");
+
+//Initial State
+const initialState = {
+    counter: [
+        {
+            idName: "counter-1",
+            counter: 0,
+        },
+    ],
+};
+
+
+
+
+
+
